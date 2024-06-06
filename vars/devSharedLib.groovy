00385b4ba61bba534f8009e5017b9a9b8766dec4
nibatandukar <nibatandukar@outlook.com>
2024-06-06 15:56:01 +0545

    Update devSharedLib.groovy

--- vars/devSharedLib.groovy
+++ vars/devSharedLib.groovy
@@ -1,5 +1,5 @@
 def BuildDev1(Map config = [:]) {
-    docker.withRegistry('https://366751107728.dkr.ecr.us-west-2.amazonaws.com', 'ecr:us-west-2:ecr-jenkins') {
+    docker.withRegistry('https://018028332614.dkr.ecr.us-east-1.amazonaws.com', 'ecr:us-east-1:fpg-prod-ecr-creds') {
         def dockerfile = "${config.Dockerfile}"
         def customImage = docker.build("jenkins-change-set:${config.tag}", "-f ./dev1/${dockerfile} ./dev1/")
         customImage.push()
@@ -7,7 +7,7 @@ def BuildDev1(Map config = [:]) {
 }
 
 def BuildDev2(Map config = [:]) {
-    docker.withRegistry('https://366751107728.dkr.ecr.us-west-2.amazonaws.com', 'ecr:us-west-2:ecr-jenkins') {
+    docker.withRegistry('https://018028332614.dkr.ecr.us-east-1.amazonaws.com', 'ecr:us-east-1:fpg-prod-ecr-creds') {
         def dockerfile = "${config.Dockerfile}"
         def customImage = docker.build("jenkins-change-set:${config.tag}", "-f ./dev2/${dockerfile} ./dev2/")
         customImage.push()
@@ -15,7 +15,7 @@ def BuildDev2(Map config = [:]) {
 }
 
 def BuildDev3(Map config = [:]) {
-    docker.withRegistry('https://366751107728.dkr.ecr.us-west-2.amazonaws.com', 'ecr:us-west-2:ecr-jenkins') {
+    docker.withRegistry('https://018028332614.dkr.ecr.us-east-1.amazonaws.com', 'ecr:us-east-1:fpg-prod-ecr-creds') {
         def dockerfile = "${config.Dockerfile}"
         def customImage = docker.build("jenkins-change-set:${config.tag}", "-f ./dev3/${dockerfile} ./dev3/")
         customImage.push()
