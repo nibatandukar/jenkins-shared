09bd1e9303853b9ca2716bad4eee5f9e9f4a06fb
nibatandukar <nibatandukar@outlook.com>
2024-06-07 18:49:46 +0545

    Update devSharedLib.groovy

--- vars/devSharedLib.groovy
+++ vars/devSharedLib.groovy
@@ -108,6 +108,10 @@ def DeployDev3(Map config = [:]) {
         
         sh """
         export KUBECONFIG=$KUBECRED
+        wget https://get.helm.sh/helm-v3.15.0-linux-arm64.tar.gz
+        tar -zxvf helm-v3.15.0-linux-amd64.tar.gz
+        mv linux-amd64/helm /usr/local/bin/helm
+        
         cd helmcharts/${config.ENV}
         helm delete dev3
         helm install dev3 dev3
